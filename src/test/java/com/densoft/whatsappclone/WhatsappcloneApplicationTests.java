d1b3fe95e5da9d1aa365594ffc708cf1685ef798
dennis githinji <wakahiad@gmail.com>
2023-09-11 23:07:38 +0300

    project init

---
+++ src/test/java/com/densoft/whatsappclone/WhatsappcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.densoft.whatsappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappcloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
